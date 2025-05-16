d40792f752cdb590fd2bb0e5a44fe79ca5a8ffd0
justmahmoud31 <justmahmoud3152003@gmail.com>
2025-05-16 16:18:15 +0300

    Fixed validator file

--- src/Modules/Products/product.validator.js
+++ src/Modules/Products/product.validator.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator';
 
 export const productValidator = [
-  body('name').withMessage('Product name is required'),
-  body('description').withMessage('Description is required'),
+  body('name').optional(),
+  body('description').optional(),
   body('price').isFloat({ gt: 0 }).withMessage('Price must be a positive number'),
   body('model').optional(),
   body('imageCover').notEmpty().withMessage('Cover image is required'),
