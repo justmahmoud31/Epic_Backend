5c470e18686b52f64c46f3053d61717136b90803
justmahmoud31 <justmahmoud3152003@gmail.com>
2025-05-16 19:23:15 +0300

    Removed : Validations

--- src/Modules/Products/product.validator.js
+++ src/Modules/Products/product.validator.js
@@ -1,12 +1,12 @@
 import { body } from 'express-validator';
 
 export const productValidator = [
-  body('name').optional(),
+  body('name').notEmpty().withMessage("name is Required"),
   body('description').optional(),
-  body('price').isFloat({ gt: 0 }).withMessage('Price must be a positive number'),
+  body('price').optional().isFloat({ gt: 0 }).withMessage('Price must be a positive number'),
   body('model').optional(),
   body('imageCover').notEmpty().withMessage('Cover image is required'),
   body('images').isArray().withMessage('Images must be an array'),
-  body('stock').isInt({ min: 0 }).withMessage('Stock must be a non-negative integer'),
+  body('stock').optional().isInt({ min: 0 }).withMessage('Stock must be a non-negative integer'),
   body('category').isMongoId().withMessage('Category must be a valid ID'),
 ];
